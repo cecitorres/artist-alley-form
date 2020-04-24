c86d44fbb488d6e46b9b077e6effcc5bc3bb3da5
Mariela1996 <mariela.778@gmail.com>
2020-04-24 14:48:33 -0500

    Local form agregado

--- src/views/localForm.vue
+++ src/views/localForm.vue
@@ -1,3 +1,146 @@
 <template>
-<h1>ALOHA</h1>
+<div class="flex flex-col">
+    <h1>
+      Formulario Artistas Locales
+    </h1>
+
+    <form class="w-full max-w-lg self-center">
+      <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0"><!--div que contiene la etiqueta + input -->
+          <label
+            class="block uppercase tracking-wide text-gray-700 text-xs
+          font-bold mb-2"
+            for="grid-first-name"
+          >
+            Nombre
+          </label>
+          <input
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="grid-first-name"
+            type="text"
+            placeholder="Escriba su nombre"
+          />
+          <!-- <p class="text-red-500 text-xs italic">Please fill out this field.</p> -->
+        </div>
+        <div class="w-full md:w-1/2 px-3">
+          <label
+            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            for="grid-last-name"
+          >
+            Edad
+          </label>
+          <input
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="grid-last-name"
+            type="text"
+            placeholder="Escriba su edad"
+          />
+        </div>
+      </div>
+       <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0"><!--div que contiene la etiqueta + input -->
+          <label
+            class="block uppercase tracking-wide text-gray-700 text-xs
+          font-bold mb-2"
+            for="grid-first-name"
+          >
+            Link del portafolio
+          </label>
+          <input
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="grid-first-name"
+            type="text"
+            placeholder="Link"
+          />
+          <!-- <p class="text-red-500 text-xs italic">Please fill out this field.</p> -->
+        </div>
+        <div class="w-full md:w-1/2 px-3">
+          <label
+            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            for="grid-last-name"
+          >
+            Correo
+          </label>
+          <input
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="grid-last-name"
+            type="text"
+            placeholder="Doe"
+          />
+        </div>
+      </div>
+       <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0"><!--div que contiene la etiqueta + input -->
+          <label
+            class="block uppercase tracking-wide text-gray-700 text-xs
+          font-bold mb-2"
+            for="grid-first-name"
+          >
+            Número de contacto
+          </label>
+          <input
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="grid-first-name"
+            type="text"
+            placeholder="Escriba su número"
+          />
+          <!-- <p class="text-red-500 text-xs italic">Please fill out this field.</p> -->
+        </div>
+        <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+          <label
+            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            for="grid-state"
+          >
+            Método de pago
+          </label>
+          <div class="relative">
+            <select
+              class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-state"
+            >
+              <option>Efectivo</option>
+              <option>Deposito</option>
+              <option>Transferencia</option>
+            </select>
+            <div
+              class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
+            >
+              <svg
+                class="fill-current h-4 w-4"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+                />
+              </svg>
+            </div>
+          </div>
+        </div> <!--state-->
+      </div>
+      <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0"><!--div que contiene la etiqueta + input -->
+          <label
+            class="block uppercase tracking-wide text-gray-700 text-xs
+          font-bold mb-2"
+            for="grid-first-name"
+          >
+            Número de Mesa
+          </label>
+          <input
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="grid-first-name"
+            type="text"
+            placeholder="Jane"
+          />
+          <!-- <p class="text-red-500 text-xs italic">Please fill out this field.</p> -->
+        </div>
+      </div>
+      <div class="mt-8">
+        <button class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+          Enviar
+        </button>
+      </div>
+    </form>
+  </div>
 </template>
